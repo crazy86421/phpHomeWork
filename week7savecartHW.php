f814b516d654e65d966ec98c54abc59eaf941fee
crazy86421 <crazy19970421@gmail.com>
2017-06-18 23:24:44 +0800

    Update and rename savecartHW.php to week7savecartHW.php

--- week7savecartHW.php
+++ week7savecartHW.php
@@ -10,5 +10,5 @@ if(isset($_SESSION["ID"])){
 	setcookie($id."[price]",$price,time()+3600);
 	setcookie($id."[quantity]",$quantity,time()+3600);
 }
-header("Location: shoppingcartHW.php")
+header("Location: week7shoppingcartHW.php")
 ?>
